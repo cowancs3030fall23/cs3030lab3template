087bac7a02f0d83b7d474f6554ad92b788d8ac7a
tcowan <tcowan@my321.net>
2021-01-07 09:41:23 -0700

    Update steps.rb
    Change all occurrences of puts to log.

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,19 +1,19 @@
 Given /^OUTPUT is printed/ do
     stdoutOutput = all_commands.map { |c| c.stdout }.join("\n").strip
     if stdoutOutput != ""
-        puts "STDOUT>>>"
-        puts stdoutOutput
-        puts "<<<STDOUT"
+        log "STDOUT>>>"
+        log stdoutOutput
+        log "<<<STDOUT"
     else
-        puts "STDOUT is EMPTY"
+        log "STDOUT is EMPTY"
     end
     stderrOutput = all_commands.map { |c| c.stderr }.join("\n").strip
     if stderrOutput != ""
-        puts "STDERR>>>"
-        puts stderrOutput
-        puts "<<<STDERR"
+        log "STDERR>>>"
+        log stderrOutput
+        log "<<<STDERR"
     else
-        puts "STDERR is EMPTY"
+        log "STDERR is EMPTY"
     end
 end
 
@@ -43,7 +43,7 @@ Given /^the output should be sorted and complete/ do
 	users = @log.users
 	#output = all_output
 	# remove the first two rows
-	#puts all_output
+	#log all_output
 	output = Array.new
 	temp_output = all_commands.map { |c| c.output }.join("\n")
 	output = temp_output.split("\n")
@@ -53,23 +53,23 @@ Given /^the output should be sorted and complete/ do
 	output.delete_at(0)
 	output.delete_at(0)
 	if users.length != output.length
-		puts "Your output:"
+		log "Your output:"
 		for i in 1..savedOutput.length
-			puts "Line #{i}: #{savedOutput[i-1]}"
+			log "Line #{i}: #{savedOutput[i-1]}"
 		end
-		puts
+		log
 		raise("#{output.length} rows of data (less the header) found in output but #{users.length} expected")
 	end
 	for i in 0..output.length-1
 		if output[i].sub(',','').match(/#{users[i].count}[ ]+#{users[i].userid}/) == nil
-			puts "Your output:"
+			log "Your output:"
 			for n in 1..savedOutput.length
-				puts "Line #{n}: #{savedOutput[n-1]}"
+				log "Line #{n}: #{savedOutput[n-1]}"
 			end
-			puts "\nExpected output (minus the header line):"
+			log "\nExpected output (minus the header line):"
 			for n in 1..users.length-1
 		        commaCount = users[n-1].count.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
-				puts "Line #{n}: #{commaCount} #{users[n-1].userid}"
+				log "Line #{n}: #{commaCount} #{users[n-1].userid}"
 			end
 		    commaCount = users[i].count.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
 			raise("'#{output[i]}' should be '#{commaCount} #{users[i].userid}'")
@@ -89,19 +89,19 @@ Given /^large numbers are punctuated with commas/ do
 	output.delete_at(0)
 	output.delete_at(0)
 	if users.length != output.length
-		puts "Your output:"
+		log "Your output:"
 		for i in 1..savedOutput.length
-			puts "Line #{i}: #{savedOutput[i-1]}"
+			log "Line #{i}: #{savedOutput[i-1]}"
 		end
-		puts
+		log
 		raise("#{output.length} rows of data (less the header) found in output but #{users.length} expected")
 	end
 	for i in 0..output.length-1
 		commaCount = users[i].count.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
 		if output[i].match(/#{commaCount}[ ]+#{users[i].userid}/) == nil
-			puts "Your output:"
+			log "Your output:"
 			for n in 1..savedOutput.length
-				puts "Line #{n}: #{savedOutput[n-1]}"
+				log "Line #{n}: #{savedOutput[n-1]}"
 			end
 			raise("'#{output[i]}' should have commas like this: '#{commaCount} #{users[i].userid}'")
 		end
@@ -111,7 +111,7 @@ end
 Given /^header ends with the current date$/ do
 	date = `date`.chomp
 	hostname = `hostname`.chomp
-	#puts "Look TED, all_output: #{all_output}"
+	#log "Look TED, all_output: #{all_output}"
 	# dates look like this: Mon May 11 14:12:24 MDT 2015
 	temp_output = all_commands.map { |c| c.output }.join("\n")
 	if !temp_output.match(/Failed Login Attempts Report as of\s+([MTWFS][a-z][a-z])\s+([JFMASOND][a-z][a-z])\s+(\d{1,2})\s+(\d\d)\:(\d\d)\:(\d\d)\s+([A-Z]{3})\s+(\d{4})/)
@@ -125,8 +125,8 @@ Given /^header ends with the current date$/ do
 	second = $6.to_i
 	hisDate = Time.new(yyyy,mon,day,hour,minute,second)
 	now = Time.now
-	#puts "His date: #{hisDate}"
-	#puts "My date: #{now}"
+	#log "His date: #{hisDate}"
+	#log "My date: #{now}"
 	#if (now - hisDate).abs > 60*60*24
 	if (now - hisDate).abs > 60 
 		raise("#{temp_output} does not contain a current date as produced by the Linux date command.  Now: #{now} His: #{hisDate}")	
@@ -135,7 +135,7 @@ end
 
 
 Given /^(.*) points are awarded/ do |points|
-	#puts "#{points} points are now awarded!!!"
+	#log "#{points} points are now awarded!!!"
 	$total_points += points.to_i
 end
 
@@ -151,7 +151,7 @@ Given /^timeout is decreased by (.*) seconds$/ do |seconds|
 	if @aruba_timeout_seconds
 		@aruba_timeout_seconds -= seconds.to_i
 	else
-		puts "aruba_timeout_seconds is NIL!"
+		log "aruba_timeout_seconds is NIL!"
 	end
 end
 
